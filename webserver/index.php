171d42e84b3d5f2e98e0d2df3702c5504bafcd6f
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-02-17 05:22:19 -0500

    update_GIT

--- webserver/index.php
+++ webserver/index.php
@@ -4,11 +4,16 @@ $conn = mysqli_connect('172.19.0.2', 'root', '12345678', 'trucorp');
 $query = "select * from users";
 $result = $conn->query($query);
 
+$count = 0;
+
 while($data=$result->fetch_assoc()){
+	$count++;
 	echo $data['id'];
 	echo $data['nama'];
 	echo $data['kantor'];
 }
+
+echo $count;
 ?>
 
 
